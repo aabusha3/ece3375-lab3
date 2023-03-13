2f666709ab661518552408eaf4668b074766276f
aabusha3 <aabusha3@uwo.ca>
2023-03-13 14:22:55 -0400

    update

--- Lab3.c
+++ Lab3.c
@@ -2,7 +2,7 @@ volatile int* LED_ptr = (int*)0xFF200000;  //lights
 volatile int*  JP1_BASE = (int*) 0xFF200060 ; //gpio lights
 volatile int*  switch_ptr = (int*) 0xFF200040; //switch pointer
 volatile int* ADC_BASE = (int*) 0xFF204000; 
-volatile int* ADC_ch1 = (int*) 0xFF204004;//where am I reading the 2 adcs from
+volatile int* ADC_ch1 = (int*) 0xFF204004;//where am I reading the 2 adcs from??
 
 void displayValue(int adcInput){
     const unsigned char ledTable[11] = {
@@ -28,14 +28,36 @@ int readSwitches(void) {
 int b15_status;
 int adcData;
 int delay;
-int mask = 1<<15;   //mask to check status bit 0x8000
+int ADC_ch2_data;   //0-15 bit data only read once 
+int mask = 1<<15;   //mask to check status bit 0x8000? 0x0001 0000?
+int switchState;
+
+void process(int word){//int converts hex to dec?
+    ADC_ch2_data = *(ADC_BASE+word) & 0xFFFF;
+    b15_status = ADC_ch2_data & mask;
+    if(b15_status == 1){
+        adcData = ADC_ch2_data & 0xFFF;
+        displayValue(adcData);
+    }
+}
+
 int main(){
     *(ADC_ch1) = 0x01;//autoupdate
     *(JP1_BASE+0x04) = 0x3FF;//set 0-9 led pins to output
     while(1){
-        b15_status = *(ADC_BASE+0x08) & mask;
+        // switchState = readSwitches();
+        // if(switchState == 0){
+        //     process(0x08);
+        // }
+        // else{
+        //     process(0x0C);
+        // }
+
+
+        ADC_ch2_data = *(ADC_BASE+0x08) & 0xFFFF;
+        b15_status = ADC_ch2_data & mask;
         if(b15_status == 1){
-            adcData = *(ADC_BASE+0x08) & 0xFFF;
+            adcData = ADC_ch2_data & 0xFFF;
             displayValue(adcData);
         }
 
